9a31760f99bf434193391dd33fd556f757f0b3a9
Mehakanis <159849439+Mehakanis@users.noreply.github.com>
2024-07-04 21:14:57 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+class Student {
+    name;
+    constructor(n) {
+        this.name = n;
+    }
+}
+class Person {
+    students = [];
+    addstudents(obj) {
+        this.students.push(obj);
+    }
+}
+const persons = new Person();
+const programStart = async (persons) => {
+    do {
+        console.log("Welcome!");
+        const ans = await inquirer.prompt({
+            name: "select",
+            type: "list",
+            message: "Whom would you like to interact with?",
+            choices: ["Staff", "Student", "Exit"]
+        });
+        if (ans.select === "Staff") {
+            console.log("You approch the staff rooom. Please feel free to ask any question.");
+        }
+        else if (ans.select === "Student") {
+            const ans = await inquirer.prompt({
+                name: "student",
+                type: "input",
+                message: "Enter Student Name, whom you wish to engage with."
+            });
+            const student = persons.students.find(val => val.name == ans.student); // input mai jo name ayega , wo ham find ker rahe ke array mai name hai ya nhi.
+            if (!student) {
+                const name = new Student(ans.student); //ager nhi hai to usko new student bana do
+                persons.addstudents(name); // array mai add kr do
+                console.log(`Hello, I am ${name.name}. Nice to meet you!`);
+                console.log("New student added");
+                console.log("Current student list:");
+                console.log(persons.students);
+            }
+            else {
+                console.log(`Hello, I am ${student.name}. `);
+                console.log("Existing student list:");
+                console.log(persons.students);
+            }
+        }
+        else if (ans.select === "Exit") {
+            console.log("Exiting the program....");
+            process.exit();
+        }
+    } while (true);
+};
+programStart(persons);
